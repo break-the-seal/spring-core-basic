69e9f9c7e43ae054db479b673392c62d65f4f42a
uyoo <hs27951@gmail.com>
2022-04-20 09:32:57 +0900

    singleton container

--- src/test/kotlin/io/brick/springcorebasic/singleton/SingletonTest.kt
+++ src/test/kotlin/io/brick/springcorebasic/singleton/SingletonTest.kt
@@ -1,9 +1,12 @@
 package io.brick.springcorebasic.singleton
 
 import io.brick.springcorebasic.AppConfig
+import io.brick.springcorebasic.member.MemberService
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 class SingletonTest {
 
@@ -34,6 +37,21 @@ class SingletonTest {
         println("singletonService2 = ${singletonService2}")
 
         Assertions.assertThat(singletonService1).isSameAs(singletonService2)
+    }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    fun springContainer() {
+        val ac: ApplicationContext = AnnotationConfigApplicationContext(AppConfig::class.java)
+
+        // 여러개의 조회 -> 요청 개수만큼 객체 생성
+        val memberService1 = ac.getBean("memberService", MemberService::class.java)
+        val memberService2 = ac.getBean("memberService", MemberService::class.java)
+
+        // 참조값이 다름
+        println("memberService1 = $memberService1")
+        println("memberService2 = $memberService2")
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2)
     }
 }
